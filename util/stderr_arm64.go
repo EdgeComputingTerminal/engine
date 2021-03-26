ad770a91a224a4e5f9cf47b3007bbc5ec058e6ec
jamiesun <jamiesun.net@gmail.com>
2021-03-26 20:04:20 +0800

    ..

--- util/stderr_arm64.go
+++ util/stderr_arm64.go
@@ -5,7 +5,6 @@ package util
 import (
 	"log"
 	"os"
-	"syscall"
 )
 
 func init() {
@@ -15,5 +14,5 @@ func init() {
 		return
 	}
 	// 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
-	syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
+	// syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
 }
